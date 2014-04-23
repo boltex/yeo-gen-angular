ee1ff4b35809e32ff78a53e55ba1c1acdc52fc8f
boltex <fmalboeuf@oceanex.com>
2014-04-23 17:59:41 -0400

    first initial commit, added animate and cleaned a bit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('trackingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
